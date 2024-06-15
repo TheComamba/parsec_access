906f24f26a9bd1d74c00e720324fb765163b285d
TheComamba <thecomamba@jojoheinze.de>
2024-06-15 12:54:06 +0200

    Extracting data.

--- scripts/generate_code.py
+++ scripts/generate_code.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import os
 import requests
+import tarfile
 
 URL = "https://people.sissa.it/~sbressan/CAF09_V1.2S_M36_LT/"
 
@@ -19,9 +20,30 @@ def collect_archive_names():
             archive_names.append(href)
     return archive_names
 
+def assure_archive_downloaded(archive_name):
+    archive_path = f'dev_data/{archive_name}'
+    if not os.path.exists(archive_path):
+        archive_url = f'{URL}{archive_name}'
+        print(f'Downloading {archive_url}...')
+        with requests.get(archive_url, stream=True) as r:
+            with open(archive_path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+def assure_extracted(archive_name):
+    archive_path = f'dev_data/{archive_name}'
+    extract_dir = archive_path.replace('.tar.gz', '')
+
+    if not os.path.exists(extract_dir):
+        with tarfile.open(archive_path, 'r:gz') as tar:
+            print(f'Extracting {archive_path}...')
+            tar.extractall(path=extract_dir)
+
 def main():
     assure_dev_data_folder()
     archive_names = collect_archive_names()
-    print(archive_names)
+    for archive_name in archive_names:
+        assure_archive_downloaded(archive_name)
+        assure_extracted(archive_name)
 
 main()
