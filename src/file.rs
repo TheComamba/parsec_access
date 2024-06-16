b5f473c98d2efe9ed1db0832efdbb7a0cf953ef7
TheComamba <thecomamba@jojoheinze.de>
2024-06-16 21:02:01 +0200

    Happy renaming.

--- src/file.rs
+++ src/file.rs
@@ -22,103 +22,111 @@ impl ParsecData {
         let file_path = data_dir.join(metallicity.to_string() + ".rmp");
 
         if file_path.exists() {
-            println!("Reading PARSEC data from {}", file_path.display());
-            let file = File::open(&file_path).map_err(ParsecAccessError::Io)?;
-            let parsec_data: ParsecData =
-                rmp_serde::from_read(file).map_err(ParsecAccessError::RmpDeserialization)?;
-            if parsec_data.is_filled() {
-                Ok(parsec_data)
-            } else {
-                Err(ParsecAccessError::DataNotAvailable(
-                    "Parsec Data".to_string(),
-                ))
-            }
+            read_existing_parsec_file(file_path)
         } else {
-            Self::ensure_data_files(&metallicity)?;
-            let data_dir_name = metallicity.to_archive_name().replace(".tar.gz", "");
-            let folder_path = data_dir.join(PathBuf::from(data_dir_name));
-            let masses = get_masses(&metallicity);
-            let filepaths = get_filenames(&metallicity);
-            let mut parsec_data = ParsecData {
-                metallicity,
-                data: Vec::with_capacity(masses.len()),
-            };
-            for mass_index in 0..masses.len() {
-                let filepath = folder_path.join(filepaths[mass_index]);
-                parsec_data.data.push(Self::read_file(filepath)?);
-            }
-            println!("Writing PARSEC data to {}", file_path.display());
-            let file = File::create(&file_path).map_err(ParsecAccessError::Io)?;
-            let buffer =
-                rmp_serde::to_vec(&parsec_data).map_err(ParsecAccessError::RmpSerialization)?;
-            let mut writer = BufWriter::new(file);
-            writer.write_all(&buffer).map_err(ParsecAccessError::Io)?;
-            Self::delete_data_files(&metallicity)?;
-            if parsec_data.is_filled() {
-                Ok(parsec_data)
-            } else {
-                Err(ParsecAccessError::DataNotAvailable(
-                    "Parsec Data".to_string(),
-                ))
-            }
+            create_parsec_data_file(metallicity, data_dir, file_path)
         }
     }
+}
 
-    fn download(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
-        let project_dirs = get_project_dirs()?;
-        let data_dir = project_dirs.data_dir();
-        let data_dir = data_dir
-            .to_str()
-            .ok_or(ParsecAccessError::Io(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Could not convert data dir to string",
-            )))?;
-        println!(
-            "Downloading PARSEC data for {} to {}",
-            metallicity, data_dir
-        );
-        let target = PARSEC_URL.to_string() + metallicity.to_archive_name();
-        let mut response = reqwest::blocking::get(target).map_err(ParsecAccessError::Connection)?;
-        let gz_decoder = GzDecoder::new(&mut response);
-        let mut archive = Archive::new(gz_decoder);
-        archive.unpack(data_dir).map_err(ParsecAccessError::Io)?;
-        Ok(())
-    }
+fn download(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
+    let project_dirs = get_project_dirs()?;
+    let data_dir = project_dirs.data_dir();
+    let data_dir = data_dir
+        .to_str()
+        .ok_or(ParsecAccessError::Io(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Could not convert data dir to string",
+        )))?;
+    println!(
+        "Downloading PARSEC data for {} to {}",
+        metallicity, data_dir
+    );
+    let target = PARSEC_URL.to_string() + metallicity.to_archive_name();
+    let mut response = reqwest::blocking::get(target).map_err(ParsecAccessError::Connection)?;
+    let gz_decoder = GzDecoder::new(&mut response);
+    let mut archive = Archive::new(gz_decoder);
+    archive.unpack(data_dir).map_err(ParsecAccessError::Io)?;
+    Ok(())
+}
 
-    fn ensure_data_files(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
-        let project_dirs = get_project_dirs()?;
-        let data_dir = project_dirs.data_dir();
-        let dirname = metallicity.to_archive_name().replace(".tar.gz", "");
-        let path = data_dir.join(PathBuf::from(dirname));
-        if !path.exists() {
-            Self::download(metallicity)?;
+fn read_trajectory_file(file_path: PathBuf) -> Result<Trajectory, ParsecAccessError> {
+    let file = File::open(file_path).map_err(ParsecAccessError::Io)?;
+    let reader = BufReader::new(file);
+    let mut lines = vec![];
+    for line in reader.lines() {
+        let line = line.map_err(ParsecAccessError::Io)?;
+        if !is_header(&line) {
+            let line = ParsedParsecLine::read(line)?;
+            lines.push(line);
         }
-        Ok(())
     }
+    Ok(Trajectory::new(lines))
+}
 
-    fn delete_data_files(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
-        let project_dirs = get_project_dirs()?;
-        let data_dir = project_dirs.data_dir();
-        let dirname = metallicity.to_archive_name().replace(".tar.gz", "");
-        let path = data_dir.join(PathBuf::from(dirname));
-        if path.exists() {
-            fs::remove_dir_all(path).map_err(ParsecAccessError::Io)?;
-        }
-        Ok(())
+fn ensure_data_files(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
+    let project_dirs = get_project_dirs()?;
+    let data_dir = project_dirs.data_dir();
+    let dirname = metallicity.to_archive_name().replace(".tar.gz", "");
+    let path = data_dir.join(PathBuf::from(dirname));
+    if !path.exists() {
+        download(metallicity)?;
     }
+    Ok(())
+}
 
-    fn read_file(file_path: PathBuf) -> Result<Trajectory, ParsecAccessError> {
-        let file = File::open(file_path).map_err(ParsecAccessError::Io)?;
-        let reader = BufReader::new(file);
-        let mut lines = vec![];
-        for line in reader.lines() {
-            let line = line.map_err(ParsecAccessError::Io)?;
-            if !is_header(&line) {
-                let line = ParsedParsecLine::read(line)?;
-                lines.push(line);
-            }
-        }
-        Ok(Trajectory::new(lines))
+fn delete_data_files(metallicity: &Metallicity) -> Result<(), ParsecAccessError> {
+    let project_dirs = get_project_dirs()?;
+    let data_dir = project_dirs.data_dir();
+    let dirname = metallicity.to_archive_name().replace(".tar.gz", "");
+    let path = data_dir.join(PathBuf::from(dirname));
+    if path.exists() {
+        fs::remove_dir_all(path).map_err(ParsecAccessError::Io)?;
+    }
+    Ok(())
+}
+
+fn create_parsec_data_file(metallicity: Metallicity, data_dir: &std::path::Path, file_path: PathBuf) -> Result<ParsecData, ParsecAccessError> {
+    ensure_data_files(&metallicity)?;
+    let data_dir_name = metallicity.to_archive_name().replace(".tar.gz", "");
+    let folder_path = data_dir.join(PathBuf::from(data_dir_name));
+    let masses = get_masses(&metallicity);
+    let filepaths = get_filenames(&metallicity);
+    let mut parsec_data = ParsecData {
+        metallicity,
+        data: Vec::with_capacity(masses.len()),
+    };
+    for mass_index in 0..masses.len() {
+        let filepath = folder_path.join(filepaths[mass_index]);
+        parsec_data.data.push(read_trajectory_file(filepath)?);
+    }
+    println!("Writing PARSEC data to {}", file_path.display());
+    let file = File::create(&file_path).map_err(ParsecAccessError::Io)?;
+    let buffer =
+        rmp_serde::to_vec(&parsec_data).map_err(ParsecAccessError::RmpSerialization)?;
+    let mut writer = BufWriter::new(file);
+    writer.write_all(&buffer).map_err(ParsecAccessError::Io)?;
+    delete_data_files(&metallicity)?;
+    if parsec_data.is_filled() {
+        Ok(parsec_data)
+    } else {
+        Err(ParsecAccessError::DataNotAvailable(
+            "Parsec Data".to_string(),
+        ))
+    }
+}
+
+fn read_existing_parsec_file(file_path: PathBuf) -> Result<ParsecData, ParsecAccessError> {
+    println!("Reading PARSEC data from {}", file_path.display());
+    let file = File::open(file_path).map_err(ParsecAccessError::Io)?;
+    let parsec_data: ParsecData =
+        rmp_serde::from_read(file).map_err(ParsecAccessError::RmpDeserialization)?;
+    if parsec_data.is_filled() {
+        Ok(parsec_data)
+    } else {
+        Err(ParsecAccessError::DataNotAvailable(
+            "Parsec Data".to_string(),
+        ))
     }
 }
 
