3dab8f3892acc4676be7061220eb7a8cfb363f34
TheComamba <thecomamba@jojoheinze.de>
2024-06-20 17:54:23 +0200

    Test for recreating the sun.

--- tests/getters.rs
+++ tests/getters.rs
@@ -3,12 +3,13 @@ mod tests {
     use parsec_access::getters::{
         get_ages_in_years, get_closest_age_index, get_closest_mass_index,
         get_closest_metallicity_index_from_mass_fraction, get_masses_in_solar,
-        get_metallicities_in_mass_fractions,
+        get_metallicities_in_mass_fractions, is_data_ready,
     };
     use simple_si_units::base::{Mass, Time};
 
     #[test]
     fn metallicites_are_mapped_to_themselves() {
+        assert!(is_data_ready());
         for (expected_index, expected_value) in
             get_metallicities_in_mass_fractions().iter().enumerate()
         {
@@ -19,6 +20,7 @@ mod tests {
 
     #[test]
     fn masses_are_mapped_to_themselves() {
+        assert!(is_data_ready());
         let metallicity_index = 3;
         for (expected_index, expected_value) in
             get_masses_in_solar(metallicity_index).iter().enumerate()
@@ -31,6 +33,7 @@ mod tests {
 
     #[test]
     fn ages_are_mapped_to_themselves() {
+        assert!(is_data_ready());
         let metallicity_index = 3;
         let mass_index = 30;
         for (expected_index, expected_value) in get_ages_in_years(metallicity_index, mass_index)
--- tests/physics.rs
+++ tests/physics.rs
@@ -1,11 +1,47 @@
 #[cfg(test)]
 mod tests {
     use parsec_access::getters::{
-        get_masses_in_solar, get_metallicities_in_mass_fractions, get_trajectory,
+        get_closest_parameters, get_masses_in_solar, get_metallicities_in_mass_fractions,
+        get_trajectory, is_data_ready,
     };
+    use simple_si_units::base::{Distance, Luminosity, Mass, Temperature, Time};
+
+    #[test]
+    fn recreating_the_sun() {
+        assert!(is_data_ready());
+        let sun_metallicity = 0.0122;
+        let sun_mass = Mass::from_solar_mass(1.);
+        let sun_age = Time::from_Gyr(4.6);
+        let sun_luminous_intensity = Luminosity::from_cd(2.98e27);
+        let sun_temperature = Temperature::from_K(5772.);
+        let sun_radius = Distance::from_km(696_300.);
+        let params = get_closest_parameters(sun_metallicity, sun_mass, sun_age);
+        assert!(
+            (params.luminous_intensity / sun_luminous_intensity - 1.).abs() < 0.15,
+            "Expected {}, got {}, which is off by a factor of {}",
+            sun_luminous_intensity,
+            params.luminous_intensity,
+            params.luminous_intensity / sun_luminous_intensity
+        );
+        assert!(
+            (params.temperature / sun_temperature - 1.).abs() < 0.15,
+            "Expected {}, got {}, which is off by a factor of {}",
+            sun_temperature,
+            params.temperature,
+            params.temperature / sun_temperature
+        );
+        assert!(
+            (params.radius / sun_radius - 1.).abs() < 0.15,
+            "Expected {}, got {}, which is off by a factor of {}",
+            sun_radius,
+            params.radius,
+            params.radius / sun_radius
+        );
+    }
 
     #[test]
     fn lifetime_mostly_decreases_with_mass() {
+        assert!(is_data_ready());
         let max_metallicity_index = get_metallicities_in_mass_fractions().len();
         for metallicity_index in 0..max_metallicity_index {
             let max_mass_index = get_masses_in_solar(metallicity_index).len();
